4354e6c2a2bcfc4593efe5af2456c301f0bd1817
Harshit Kasera <harshitkasera01@gmail.com>
2026-02-20 13:17:57 +0530

    change webs

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -6,7 +6,12 @@ const Footer = () => {
       <h3 className="fhead">HK Digital</h3>
       <p className="fpara">Premium Website & Growth Systems for Local Businesses</p>
       <p className="fpara">📍 Shajapur (M.P.)</p>
-      <p className="fpara">📞 9302252353</p>
+     <a href="tel:+919302252353" className="fpara">
+  📞 9302252353
+</a>
+      <a href="https://mail.google.com/mail/u/0/#inbox" className="fpara">📩 harshitkasera01@gmail.com</a>
+      <a href="https://www.instagram.com/_kasera_h353/" className="fpara"> Instagram</a>
+      
       <p fpara>© 2026 HK Digital</p>
     </footer>
   );
